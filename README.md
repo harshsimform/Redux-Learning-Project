6a40010f1290f11bf51e453dbe979d2456d5f02e
harshsimform <harsh.v@simformsolutions.com>
2023-05-04 11:16:36 +0530

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Redux Learning with Project
+
+- Link:- [Click here](https://react-redux-app-crud.netlify.app/)
--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -6,36 +6,60 @@ const CustomModal = ({ id }) => {
   const singleUser = allUsers.filter((ele) => ele.id === id);
   //   console.log(singleUser);
 
+  // const handleClose = () => setShow(false);
+
   return (
-    <div
-      className="modal fade"
-      id="exampleModal"
-      tabIndex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">
-              User Details
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <p>Name: {singleUser[0].name}</p>
-            <p>Email: {singleUser[0].email}</p>
-            <p>Age: {singleUser[0].age}</p>
-            <p>Gender: {singleUser[0].gender}</p>
+    <>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                User Details
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Name: {singleUser[0].name}</p>
+              <p>Email: {singleUser[0].email}</p>
+              <p>Age: {singleUser[0].age}</p>
+              <p>Gender: {singleUser[0].gender}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. even try to press escape
+          key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal> */}
+    </>
   );
 };
 
